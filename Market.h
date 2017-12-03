7ddd3ba0e843a2e824eba7bed868dc9ef8752996
dlut414 <hfy8373@gmail.com>
2017-12-04 00:00:37 +0900

    wrote to class Market

--- Market.h
+++ Market.h
@@ -7,7 +7,7 @@
 #include <pair>
 
 enum Commodity {};
-typedef std::pair<int,int> Pii;
+typedef std::pair<int,int> Pii; //nunber -> price
 typedef std::pair<Commodity,Pii> Order_t;
 
 class Market{
@@ -15,10 +15,18 @@ public:
 	typedef std::unordered_map<Commodity,std::vector<Pii>> OrderBook_t;
 	Market(){}
 	~Market(){}
-	bool setBidOrder(const Order_t& order){}
-	bool setAskOrder(const Order_t& order){}
-	const OrderBook_t& getBidOrder()const{}
-	const OrderBook_t& getAskOrder()const{}
+	bool setBidOrder(const Order_t& order){
+		bidOrder[order.first].push_back(order.second);
+	}
+	bool setAskOrder(const Order_t& order){
+		askOrder[order.first].push_back(order.second);
+	}
+	const OrderBook_t& getBidOrder()const{
+		return bidOrder;
+	}
+	const OrderBook_t& getAskOrder()const{
+		return askOrder;
+	}
 private:
 	OrderBook_t bidOrder;
 	OrderBook_t askOrder;
