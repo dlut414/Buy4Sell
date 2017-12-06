1930b4e22c828e3ee02b0483793695eb3a5f59bc
dlut414 <hfy8373@gmail.com>
2017-12-06 14:58:58 +0900

    rewrote doTransaction function

--- Market.h
+++ Market.h
@@ -20,8 +20,8 @@ class Market : public Log{
 public:
 	typedef Commodity Cmt;
 	typedef Agent<Market<Cmt>>* AgentHandle_t;
-	typedef tuple<Cmt,int,int> Order_t; //Commodity, number, price
-	typedef unordered_map<Cmt,vector<tuple<AgentHandle_t,int,int>>> OrderBook_t; //Commodity -> vector(handle, number, price)
+	typedef tuple<uuid,AgentHandle_t,int,int> Order_t; //0:uuid, 1:AgentHandle_t, 2:number, 3:price
+	typedef unordered_map<Cmt,vector<Order_t>> OrderBook_t; //Commodity -> vector(Order_t)
 	explicit Market(){}
 	~Market(){}
 	bool doTransaction(){
@@ -31,29 +31,29 @@ public:
 				auto& bidv = it->second;
 				auto& askv = askOrder[it->first];
 				if(bidv.empty() || askv.empty()) continue;
-				sort(bidv.begin(), bidv.end(), [](const auto& a, const auto& b){ return get<2>(a) < get<2>(b); });
-				sort(askv.begin(), askv.end(), [](const auto& a, const auto& b){ return get<2>(a) < get<2>(b); });
-				for(size_t i=bidv.size()-1, j=0;i>=0;j<ask.size();i--,j++){
-					if(get<2>(bidv[i]) < get<2>(askv[j])) break;
-					auto& nbid = get<1>(bidv[i]);
-					auto& nask = get<1>(askv[j]);
+				sort(bidv.begin(), bidv.end(), [](const auto& a, const auto& b){ return get<3>(a) < get<3>(b); });
+				sort(askv.begin(), askv.end(), [](const auto& a, const auto& b){ return get<3>(a) < get<3>(b); });
+				for(size_t i=bidv.size()-1, j=0;i>=0&&j<ask.size();i--,j++){
+					if(get<3>(bidv[i]) < get<3>(askv[j])) break;
+					auto& nbid = get<2>(bidv[i]);
+					auto& nask = get<2>(askv[j]);
 					const int volume = min(nbid, nask);
-					const int price = (get<2>(bidv[i]) + get<2>(askv[j])) / 2;
-					bool b = get<0>(bidv[i])->dealBid(make_tuple(it->first, volume, price));
-					bool a = get<0>(askv[j])->dealAsk(make_tuple(it->first, volume, price));
+					const int price = (get<3>(bidv[i]) + get<3>(askv[j])) / 2;
+					bool b = get<1>(bidv[i])->dealBid(bidv[i]);
+					bool a = get<1>(askv[j])->dealAsk(askv[j]);
 					nbid -= volume;
 					nask -= volume;
 					if(nbid != 0) i++;
 					if(nask != 0) j--;
 				}
 				for(size_t i=0;i<bidv.size();i++){
-					if(get<1>(bidv[i]) == 0) {
+					if(get<2>(bidv[i]) == 0) {
 						bidv.erase(bidv.begin() + i);
 						i--;
 					}
 				}
 				for(size_t i=0;i<askv.size();i++){
-					if(get<1>(askv[i]) == 0) {
+					if(get<2>(askv[i]) == 0) {
 						askv.erase(askv.begin() + i);
 						i--;
 					}
