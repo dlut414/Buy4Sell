5df25eb2e4ffb742a38bf056b8c1e43a833972d9
dlut414 <hfy8373@gmail.com>
2017-12-07 14:46:18 +0900

    wrote test code in main(); passed tests

--- Buy4Sell.cpp
+++ Buy4Sell.cpp
@@ -3,6 +3,7 @@
 ///2017.11~
 #include <iostream>
 #include <vector>
+#define DEBUG
 #include "Market.h"
 #include "Agent.h"
 using namespace std;
@@ -12,5 +13,8 @@ int main(){
 	Mkt mkt;
 	Agent<Mkt> agt1(mkt, vector<Cmt>{1, 3}, vector<Cmt>{1, 1, 2, 2, 3, 3, 4, 4, 5, 5});
 	Agent<Mkt> agt2(mkt, vector<Cmt>{3, 4}, vector<Cmt>{1, 1, 2, 2, 3, 3, 4, 4, 5, 5});
+	agt1.ask(4, 2, 10);
+	agt2.bid(4, 1, 16);
+	cout << "do transaction: " << mkt.doTransaction() << endl;
 	return 0;
 }
